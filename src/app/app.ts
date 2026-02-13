76b4505037f83677b5eed208f00d826fe2bbb357
Pablo Alan <pabloxt14@gmail.com>
2026-02-13 19:14:09 -0300

    feat: create certificate-item

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,18 @@ import { CommonModule } from "@angular/common"
 import { Navbar } from "./components/navbar/navbar"
 import { PrimaryButton } from "./components/primary-button/primary-button"
 import { SecondaryButton } from "./components/secondary-button/secondary-button"
+import { CertificateItem } from "./components/certificate-item/certificate-item"
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    CommonModule,
+    PrimaryButton,
+    SecondaryButton,
+    CertificateItem,
+  ],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
