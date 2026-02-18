1cc984916b7bd1f55f2fb689c06b9764aea03fdb
Pablo Alan <pabloxt14@gmail.com>
2026-02-18 16:59:24 -0300

    feat: use router-link to navigate between pages

--- src/app/components/certificate-item/certificate-item.ts
+++ src/app/components/certificate-item/certificate-item.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core"
 import { SecondaryButton } from "../secondary-button/secondary-button"
+import { RouterLink } from "@angular/router"
 
 @Component({
   selector: "app-certificate-item",
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: "./certificate-item.html",
   styleUrl: "./certificate-item.css",
 })
-export class CertificateItem {}
+export class CertificateItem {
+  id = 1
+}
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,10 @@
-import { Component, type OnInit } from "@angular/core"
+import { Component } from "@angular/core"
+import { RouterLink } from "@angular/router"
 
 @Component({
   selector: "app-navbar",
-  imports: [],
+  imports: [RouterLink],
   templateUrl: "./navbar.html",
   styleUrl: "./navbar.css",
 })
-export class Navbar implements OnInit {
-  ngOnInit(): void {
-    this.message()
-  }
-
-  message() {
-    console.log("Navbar initialized!")
-  }
-}
+export class Navbar {}
