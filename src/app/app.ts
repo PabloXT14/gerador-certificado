07e91dd2b88b38de352f54269bd76940ba8155be
Pablo Alan <pabloxt14@gmail.com>
2026-02-19 17:51:20 -0300

    refactor: remove unused code

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,21 +4,10 @@ import { CommonModule } from "@angular/common"
 
 import { Navbar } from "./components/navbar/navbar"
 import { BaseLayout } from "./components/base-layout/base-layout"
-import { Certificates } from "./pages/certificates/certificates"
-import { CertificateForm } from "./pages/certificate-form/certificate-form"
-import { Certificate } from "./pages/certificate/certificate"
 
 @Component({
   selector: "app-root",
-  imports: [
-    RouterOutlet,
-    Navbar,
-    CommonModule,
-    BaseLayout,
-    Certificates,
-    CertificateForm,
-    Certificate,
-  ],
+  imports: [RouterOutlet, Navbar, CommonModule, BaseLayout],
   templateUrl: "./app.html",
   styleUrl: "./app.css",
 })
