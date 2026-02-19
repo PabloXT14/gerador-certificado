45b331ebfd49d71a13593e372e16ef26c07f8ff8
Pablo Alan <pabloxt14@gmail.com>
2026-02-19 17:08:36 -0300

    feat: start use ng-model on form

--- src/app/pages/certificate-form/certificate-form.ts
+++ src/app/pages/certificate-form/certificate-form.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core"
+import { FormsModule } from "@angular/forms"
+
 import { PrimaryButton } from "@/app/components/primary-button/primary-button"
 import { SecondaryButton } from "@/app/components/secondary-button/secondary-button"
 
 @Component({
   selector: "app-certificate-form",
-  imports: [PrimaryButton, SecondaryButton],
+  imports: [PrimaryButton, SecondaryButton, FormsModule],
   templateUrl: "./certificate-form.html",
   styleUrl: "./certificate-form.css",
 })
-export class CertificateForm {}
+export class CertificateForm {
+  studentName = ""
+  activityName = ""
+  activities: string[] = ["Atividade 1", "Atividade 2", "Atividade 3"]
+}
