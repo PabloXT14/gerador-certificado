c7c2c0cc369985e9ba1e219c6c8e433a43cb2246
Pablo Alan <pabloxt14@gmail.com>
2026-02-26 18:58:53 -0300

    feat: create download feature

--- src/app/pages/certificate/certificate.ts
+++ src/app/pages/certificate/certificate.ts
@@ -1,8 +1,14 @@
-import { Component, type OnInit } from "@angular/core"
+import {
+  Component,
+  type ElementRef,
+  ViewChild,
+  type OnInit,
+} from "@angular/core"
 import { SecondaryButton } from "@/app/components/secondary-button/secondary-button"
 // biome-ignore lint/style/useImportType: need to import the type for the constructor
 import { ActivatedRoute, RouterLink } from "@angular/router"
 import dayjs from "dayjs"
+import html2canvas from "html2canvas"
 
 // biome-ignore lint/style/useImportType: need to import the type for the constructor
 import { CertificateService } from "@/app/services/certificate"
@@ -19,6 +25,7 @@ export class CertificateComponent implements OnInit {
   certificateId: string | null = null
   certificate: Certificate | undefined
   formattedDate = ""
+  @ViewChild("certificateContainer") certificateContainer!: ElementRef
 
   constructor(
     private certificateService: CertificateService,
@@ -41,5 +48,22 @@ export class CertificateComponent implements OnInit {
         )
       }
     })
+  }
+
+  downloadCertificate() {
+    if (!this.certificate) {
+      return
+    }
+
+    html2canvas(this.certificateContainer.nativeElement, { scale: 2 }).then(
+      (canvas) => {
+        const img = canvas.toDataURL("image/png") // convert canvas to image
+        const link = document.createElement("a") // create download link
+
+        link.download = `certificado-${this.certificate?.studentName.toLocaleLowerCase().replaceAll(" ", "-")}.png` // set download name
+        link.href = img // set image source
+        link.click() // trigger download
+      }
+    )
   }
 }
